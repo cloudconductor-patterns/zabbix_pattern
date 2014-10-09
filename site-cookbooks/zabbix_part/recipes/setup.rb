7c556088a5d7cb5c6c8cc8779899aedc7767afda
maruoka takashi <maruoka.takashi@tis.co.jp>
2014-10-09 07:30:35 +0100

    Modified auto_registration action to run from setup recipe

--- site-cookbooks/zabbix_part/recipes/setup.rb
+++ site-cookbooks/zabbix_part/recipes/setup.rb
@@ -19,3 +19,19 @@ include_recipe 'zabbix::server'
 include_recipe 'zabbix::web'
 include_recipe 'apache2::mod_php5'
 include_recipe 'zabbix_part::import_template'
+
+if node['zabbix']['web']['fqdn']
+  zabbix_server = node
+else
+  Chef::Log.warn('This recipe uses search. Chef Solo does not support search.')
+  Chef::Log.warn("If you did not set node['zabbix']['web']['fqdn'], the recipe will fail")
+  return
+end
+
+zabbix_part_auto_registration 'auto_registration_action_sample' do
+  action :create
+  zabbix_fqdn zabbix_server['zabbix']['web']['fqdn']
+  login       zabbix_server['zabbix']['web']['login']
+  password    zabbix_server['zabbix']['web']['password']
+  template    'Template OS Linux'
+end
