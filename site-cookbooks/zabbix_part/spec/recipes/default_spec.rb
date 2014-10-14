b89394a704d205728a7c88a26fb66d1cf8b80e2b
maruoka takashi <maruoka.takashi@tis.co.jp>
2014-10-14 06:33:07 +0100

    Fix rubocop violation

--- site-cookbooks/zabbix_part/spec/recipes/default_spec.rb
+++ site-cookbooks/zabbix_part/spec/recipes/default_spec.rb
@@ -3,19 +3,18 @@ require_relative '../spec_helper'
 describe 'zabbix_part::default' do
   let(:chef_run) do
     ChefSpec::Runner.new(
-      cookbook_path: ['site-cookbooks', 'cookbooks'],
+      cookbook_path: %w(site-cookbooks cookbooks),
       platform:      'centos',
       version:       '6.5'
     ).converge(described_recipe)
   end
 
   before do
-    stub_command("/usr/sbin/httpd -t").and_return(0)
-    stub_command("which php").and_return(0)
+    stub_command('/usr/sbin/httpd -t').and_return(0)
+    stub_command('which php').and_return(0)
   end
 
   it 'include recipe zabbix_part::setup' do
     expect(chef_run).to include_recipe 'zabbix_part::setup'
   end
 end
-
