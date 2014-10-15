0d376c2a54e7740337d6235b678f23b2ae2c746c
Takashi Maruoka <maruoka.takashi@tis.co.jp>
2014-10-15 13:22:48 +0900

    Add serverspec helper files

---
+++ serverspec/spec/spec_helper.rb
@@ -0,0 +1,20 @@
+require 'serverspec'
+
+include SpecInfra::Helper::Exec
+include SpecInfra::Helper::DetectOS
+
+require 'consul_parameters'
+
+include ConsulParameters
+properties = { consul_parameters: read, servers: read_servers }
+
+RSpec.configure do |c|
+  if ENV['ASK_SUDO_PASSWORD']
+    require 'highline/import'
+    c.sudo_password = ask('Enter sudo password: ') { |q| q.echo = false }
+  else
+    c.sudo_password = ENV['SUDO_PASSWORD']
+  end
+
+  set_property properties
+end
