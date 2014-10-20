50290df7e01fc8ad318199aa8a7dbf7ba380a532
maruoka takashi <maruoka.takashi@tis.co.jp>
2014-10-20 05:55:30 +0100

    Add default fqdn on default attribute

--- site-cookbooks/zabbix_part/attributes/default.rb
+++ site-cookbooks/zabbix_part/attributes/default.rb
@@ -1,5 +1,6 @@
 default['zabbix']['server']['version'] = '2.2.6'
 default['zabbix']['web']['install_method'] = 'apache'
+default['zabbix']['web']['fqdn'] = 'localhost'
 default['zabbix']['database']['dbpassword'] = 'ilikerandompasswords'
 default['mysql']['version'] = '5.6'
 default['zabbix_part']['auto_registration']['template'] = 'Template OS Linux'
