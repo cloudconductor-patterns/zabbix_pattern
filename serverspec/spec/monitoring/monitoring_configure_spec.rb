e4dadcb3d4f488963e8f81c05197f3cc45482847
b03b88c9a1df <b03b88c9a1df@gmail.com>
2015-03-18 04:12:46 +0000

    Fix serverspec to ignore http_proxy.

--- serverspec/spec/monitoring/monitoring_configure_spec.rb
+++ serverspec/spec/monitoring/monitoring_configure_spec.rb
@@ -18,14 +18,8 @@ describe service('zabbix_server') do
 end
 
 describe 'zabbix server example' do
-  before do
-    @http_proxy = ENV['http_proxy']
-    ENV['http_proxy'] = nil
-  end
-
-  after do
-    ENV['http_proxy'] = @http_proxy
-  end
+  http_proxy = ENV['http_proxy']
+  ENV['http_proxy'] = nil
 
   params = property[:consul_parameters]
 
@@ -47,6 +41,7 @@ describe 'zabbix server example' do
   end
 
   zabbix_client = CloudConductor::ZabbixClient.new(server, user, passwd)
+  ENV['http_proxy'] = http_proxy
   servers = property[:servers]
   servers.each_key do |hostname|
     result = zabbix_client.exist_host("#{hostname}")
