c156c0b3cb9fa66d30247760f1c408d4307a464f
Yo Takezawa <takezawa.yo@tis.co.jp>
2015-09-04 09:15:05 +0900

    Read chef attributes with new format from consul KVS

--- serverspec/spec/consul_parameters.rb
+++ serverspec/spec/consul_parameters.rb
@@ -11,10 +11,11 @@ module ConsulParameters
     parameters = {}
     begin
       consul_secret_key = ENV['CONSUL_SECRET_KEY'].nil? ? '' : CGI.escape(ENV['CONSUL_SECRET_KEY'])
-      response = Net::HTTP.get URI.parse("http://localhost:8500/v1/kv/cloudconductor/parameters?token=#{consul_secret_key}")
-      response_hash = JSON.parse(response, symbolize_names: true).first
-      parameters_json = Base64.decode64(response_hash[:Value])
-      parameters = JSON.parse(parameters_json, symbolize_names: true)
+      response = Net::HTTP.get URI.parse("http://localhost:8500/v1/kv/cloudconductor?recurse&token=#{consul_secret_key}")
+      JSON.parse(response, symbolize_names: true).each do |response_hash|
+        parameters_json = Base64.decode64(response_hash[:Value])
+        parameters = parameters.deep_merge(JSON.parse(parameters_json, symbolize_names: true))
+      end
     rescue => exception
       p exception.message
     end
