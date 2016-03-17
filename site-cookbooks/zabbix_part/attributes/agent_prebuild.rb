39fa8de76a03737ea3183a893c6c0d596a45fc71
bsmile <battlesmile@gmail.com>
2016-03-17 16:42:10 +0900

    modify zabbix-agent checksum

--- site-cookbooks/zabbix_part/attributes/agent_prebuild.rb
+++ site-cookbooks/zabbix_part/attributes/agent_prebuild.rb
@@ -2,4 +2,4 @@ include_attribute 'zabbix_part::agent'
 
 default['zabbix']['agent']['prebuild']['arch']  = node['kernel']['machine'] == 'x86_64' ? 'amd64' : 'i386'
 default['zabbix']['agent']['prebuild']['url']      = "http://www.zabbix.com/downloads/#{node['zabbix']['agent']['version']}/zabbix_agents_#{node['zabbix']['agent']['version']}.linux2_6.#{node['zabbix']['agent']['prebuild']['arch']}.tar.gz"
-default['zabbix']['agent']['checksum'] = 'ec3d19dcdf484f60bc4583a84a39a3bd59c34ba1e7f8abf9438606eb14b90211'
+default['zabbix']['agent']['checksum'] = 'b3e37a5126173cac211d96d791ba1046cec0764d6aa1b8ad75e343d5a2543dda'
